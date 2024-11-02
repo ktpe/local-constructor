15328485a59af55d566e34b804e19821a4c23630
Artem Stepanchenko <astepanchenko1@gmail.com>
2024-11-02 18:52:54 +0200

    added field_tyle_controller.js

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,4 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@oddcamp/cocoon-vanilla-js", to: "@oddcamp--cocoon-vanilla-js.js" # @1.1.3
+pin "controllers", to: "controllers/index.js"
