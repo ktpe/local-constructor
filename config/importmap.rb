54db98c49230a5133282b4fb6b59e1b8d686d16d
Artem Stepanchenko <astepanchenko1@gmail.com>
2024-11-02 19:08:28 +0200

    updated application.html importmap config

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,5 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "@oddcamp/cocoon-vanilla-js", to: "@oddcamp--cocoon-vanilla-js.js" # @1.1.3
+pin "@oddcamp/cocoon-vanilla-js", to: "https://ga.jspm.io/npm:@oddcamp/cocoon-vanilla-js@1.1.3/index.js" # @1.1.3
 pin "controllers", to: "controllers/index.js"
