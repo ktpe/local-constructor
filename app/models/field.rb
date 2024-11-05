4fea621bdecb9ebd5994819b8ae7106210f99e01
Bohdan <dennger228@gmail.com>
2024-11-05 17:38:36 +0200

    Changed error message in field validation

--- app/models/field.rb
+++ app/models/field.rb
@@ -26,7 +26,7 @@ class Field < ApplicationRecord
   def field_is_used_in_any_formula
     calculator.formulas.each do |formula|
       unless formula.expression.scan(/\b[a-zA-Z_]\w*\b/).uniq.include?(var_name)
-        errors.add(:var_name, "field isn't used in any formulas")
+        errors.add(:var_name, "#{var_name} isn't part of any formula")
         return
       end
     end
