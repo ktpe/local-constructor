c5f26aae6b1e2beda6a30536fe732e3398f009de
Bohdan <dennger228@gmail.com>
2024-11-05 12:26:46 +0200

    Added validations for the calculator where formulas must have all fields included and fields must be included in formulas

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -12,4 +12,33 @@ class Calculator < ApplicationRecord
   has_many :formulas, dependent: :destroy
   accepts_nested_attributes_for :fields, reject_if: :all_blank, allow_destroy: true
   accepts_nested_attributes_for :formulas, reject_if: :all_blank, allow_destroy: true
+
+  validate :formulas_include_all_fields
+  validate :fields_are_included_in_formulas
+
+  private
+
+  def formulas_include_all_fields
+    formulas.each do |formula|
+      missing_var_names = fields.map(&:var_name).reject { |var_name| formula.expression.include?(var_name) }
+
+      if missing_var_names.present?
+        errors.add(:base, "Fields #{missing_var_names.join(', ')} aren't used in any formulas")
+      end
+    end
+  end
+
+  def fields_are_included_in_formulas
+    field_names = fields.map(&:var_name)
+
+    formula_variables = formulas.flat_map do |formula|
+      formula.expression.scan(/\b[a-zA-Z_]\w*\b/)
+    end.uniq
+
+    unused_fields = formula_variables.reject { |var| field_names.include?(var) }
+
+    if unused_fields.any?
+      errors.add(:base, "The #{unused_fields.join(', ')} fields are not initialized")
+    end
+  end
 end
